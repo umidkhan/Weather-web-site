16137ecab792dd4b926773c14328b0bdbf2202ea
umidkhan <gameblogger924@gmail.com>
2022-08-10 15:36:00 +0500

    💻💻💻💻

---
+++ script.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('.input_text');
+let main = document.querySelector('#name');
+let temp = document.querySelector('.temp');
+let desc = document.querySelector('.desc');
+let clouds = document.querySelector('.clouds');
+let button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(e){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
+.then(response => response.json())
+.then(data => {
+  let tempValue = data['main']['temp'];
+  let nameValue = data['name'];
+  let descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
+})
